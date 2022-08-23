468636e6e4c066ed63549447dfc8431297fc1b15
n27 <arollingstone27@gmail.com>
2022-08-24 00:06:21 +0900

    useEffect return

--- 리액트를다루는기술/chapter2/src/App.js
+++ 리액트를다루는기술/chapter2/src/App.js
@@ -1,11 +1,24 @@
 import Counter from "./Counter";
 import Info from "./Info";
+import { useState } from 'react';
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <>
-      <Counter />    
-      <Info />
+      <div>
+        <button
+          onClick={() => { setVisible(!visible); }}
+        >
+          {visible ? '숨기기' : '보이기'}
+        </button>
+      </div>
+      <hr />
+      {visible && <>
+        <Counter />          
+        <Info />
+      </>
+      }      
     </>
   );
 }
--- 리액트를다루는기술/chapter2/src/Info.js
+++ 리액트를다루는기술/chapter2/src/Info.js
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const Info = () => {
 
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  // 기본적으로 렌더링 될때마다 실행
+  // ,[] 빈 배열 추가하면 업데이트 때는 실행 x
+  // 특정 값이 업데이트될 때만 실행하고 싶을때는 배열안에 체크하고 싶은 값을 넣으면 됨
+  useEffect(() => {
+    console.log('렌더링이 완료되었습니다!');
+    console.log({
+      name,
+      nickname
+    });
+    // 업데이트 전, 혹은 unmount
+    // unmount만 실행하고 싶으면 빈 배열
+    return () => {
+      console.log('cleanup', name);
+    }
+
+  },[]);
 
   const onChangeName = (e) => {
     setName(e.target.value);    
