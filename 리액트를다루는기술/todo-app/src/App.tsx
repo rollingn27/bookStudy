3d1f6d4c61bede298f46f0c00f0871bfacce4ade
DESKTOP-O9AU6CA\rollingstone <aRollingStone27@gmail.com>
2022-08-24 22:25:40 +0900

    최적화 작업 1단계 많은 더미 데이터 만들기

--- 리액트를다루는기술/todo-app/src/App.tsx
+++ 리액트를다루는기술/todo-app/src/App.tsx
@@ -5,13 +5,29 @@ import TodoList from './todo/TodoList';
 import TodoTemplate from './todo/TodoTemplate';
 import { todo } from './todo/TodoList';
 
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
+      checked: false,
+    });
+  }
+  return array;
+}
+
 function App() {
-  const initialTodos = [
-    { id: 1, text: '리액트 기초 알아보기', checked: true },
-    { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
-    { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
-  ];
-  const [todos, setTodos] = useState(initialTodos);
+  // const initialTodos = [
+  //   { id: 1, text: '리액트 기초 알아보기', checked: true },
+  //   { id: 2, text: '컴포넌트 스타일링해 보기', checked: true },
+  //   { id: 3, text: '일정 관리 앱 만들어 보기', checked: false },
+  // ];
+
+  // 더미 데이터 만들기
+  // 주의할점
+  // const [todos, setTodos] = useState(createBulkTodos()); 이렇게 넣으면 리렌더링 될 때마다 호출함
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고윳값으로 사용될 id
   const nextId = useRef(4);
