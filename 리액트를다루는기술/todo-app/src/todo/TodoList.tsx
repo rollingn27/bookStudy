3f505537f41fea4604ac0644ebd7f6e7b879b7dd
n27 <arollingstone27@gmail.com>
2022-08-26 12:54:03 +0900

    react-virtualized 활용 무한 스크롤 구현,

--- 리액트를다루는기술/todo-app/src/todo/TodoList.tsx
+++ 리액트를다루는기술/todo-app/src/todo/TodoList.tsx
@@ -1,6 +1,7 @@
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
-import React from 'react';
+import React, { CSSProperties, useCallback } from 'react';
+import { List } from 'react-virtualized';
 
 export type todo = {
   id: number;
@@ -14,15 +15,35 @@ type todoListProps = {
   onToggle: Function;
 };
 
+type list = {
+  index: number;
+  key: string;
+  style: CSSProperties;
+};
 const TodoList = ({ todos, onRemove, onToggle }: todoListProps) => {
+  const rowRenderer = useCallback(
+    ({ index, key, style }: list) => {
+      const todo = todos[index];
+      return <TodoListItem todo={todo} key={key} onRemove={onRemove} onToggle={onToggle} style={style} />;
+    },
+    [onRemove, onToggle, todos],
+  );
+
   return (
-    <div className="TodoList">
-      {todos.map((todo: todo) => (
-        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} onToggle={onToggle} />
-      ))}
-    </div>
+    <>
+      <List
+        className="TodoList"
+        width={512}
+        height={513}
+        rowCount={todos.length}
+        rowHeight={57}
+        rowRenderer={rowRenderer}
+        list={todos}
+        style={{ outline: 'none' }}
+      />
+    </>
   );
 };
 
-// 현재는 불필요한 렌더링은 발생하지 않음. 
+// 현재는 불필요한 렌더링은 발생하지 않음.
 export default React.memo(TodoList);
--- 리액트를다루는기술/todo-app/src/todo/TodoListItem.tsx
+++ 리액트를다루는기술/todo-app/src/todo/TodoListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from 'react-icons/md';
 import { todo } from './TodoList';
 import './TodoListItem.scss';
@@ -8,18 +8,21 @@ type todoListItemProps = {
   todo: todo;
   onRemove: Function;
   onToggle: Function;
+  style: CSSProperties;
 };
 
-const TodoListItem = ({ todo, onRemove, onToggle }: todoListItemProps) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }: todoListItemProps) => {
   const { id, text, checked } = todo;
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={() => onRemove(id)}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={() => onRemove(id)}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
