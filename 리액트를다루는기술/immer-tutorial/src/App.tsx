d10058be07c4d536f031e0846889521766ba5dcc
n27 <arollingstone27@gmail.com>
2022-08-27 17:44:16 +0900

    복잡한 state 불변성 유지(immer 적용전)

---
+++ 리액트를다루는기술/immer-tutorial/src/App.tsx
@@ -0,0 +1,86 @@
+import React, { useRef, useCallback, useState, ReactEventHandler, ReactHTMLElement } from 'react';
+import './App.css';
+
+type info = {
+  id: number;
+  name: string;
+  username: string;
+};
+
+function App() {
+  const nextId = useRef(1);
+  const initialForm = { name: '', username: '' };
+  const initialData = { array: [] as info[], uselessValue: null };
+  const [form, setForm] = useState(initialForm);
+  const [data, setData] = useState(initialData);
+
+  // input 수정을 위한 함수
+  const onChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+      setForm({
+        ...form,
+        [name]: [value],
+      });
+    },
+    [form],
+  );
+
+  // form 등록을 위한 함수
+  const onSubmit = useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      };
+
+      // array에 새 항목 등록
+      setData({
+        ...data,
+        array: data.array.concat(info),
+      });
+
+      // form 초기화
+      setForm({
+        name: '',
+        username: '',
+      });
+      nextId.current += 1;
+    },
+    [data, form.name, form.username],
+  );
+
+  // 항목을 삭제하는 함수
+  const onRemove = useCallback(
+    (id: number) => {
+      setData({
+        ...data,
+        array: data.array.filter((info: info) => info.id !== id),
+      });
+    },
+    [data],
+  );
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
+        <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info: info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
