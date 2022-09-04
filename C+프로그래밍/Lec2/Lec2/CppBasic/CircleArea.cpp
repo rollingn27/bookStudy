58c2bd4d5063836fd7502031c6cd72deb4d5089b
n27 <arollingstone27@gmail.com>
2022-09-04 10:10:38 +0900

    lec2 흐름제어구문

--- C+프로그래밍/Lec2/Lec2/CppBasic/CircleArea.cpp
+++ C+프로그래밍/Lec2/Lec2/CppBasic/CircleArea.cpp
@@ -12,5 +12,79 @@ int main()
 	cin >> radius;
 	double area = radius * radius * PI;
 	cout << "원의 면적 = " << area << endl;
+
+	// if문
+	int ifA{ 10 };
+	int ifB{ 15 };
+
+	if (ifA < ifB)
+	{
+		int t = ifA;
+		ifA = ifB;
+		ifB = t;
+	}
+
+
+	// switch문
+	int score;
+	char grade;
+
+	cin >> score;
+
+	switch (score / 10)
+	{
+	case 10:
+	case 9:
+		grade = 'A';
+		break;
+	case 8:
+		grade = 'B';
+		break;
+	case 7:
+		grade = 'C';
+		break;
+	case 6:
+		grade = 'D';
+		break;
+	default:
+		grade = 'F';
+	}
+
+	//for문
+	int val, total = 0;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> val;
+		total += val;
+	}
+
+	// 범위 for문
+	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int sum = 0;
+	for (int a : arr)
+	{
+		sum += a;
+	}
+
+	cout << "합계 = " << sum << endl;
+
+	// 참조
+
+	for (int a : arr)
+	{
+		cout << "참조전 arr " << a;
+	}
+	cout << "-----------------------" << endl;		
+
+	for (int &a : arr)
+	{
+		++a;
+	}
+
+	for (int a : arr)
+	{
+		cout << "참조후 arr " << a;
+	}
+
 	return 0;
 }
