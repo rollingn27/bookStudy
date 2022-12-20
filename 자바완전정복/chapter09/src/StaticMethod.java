4c986ad35597c0de1aa056bdf4cad82e04729d44
n27 <arollingstone27@gmail.com>
2022-12-21 08:39:06 +0900

    chapter09

---
+++ 자바완전정복/chapter09/src/StaticMethod.java
@@ -0,0 +1,22 @@
+package ex.aaa;
+class A {
+    void abc () {
+        System.out.println("instance 메서드");
+    }
+    static void bcd() {
+        System.out.println("static 메서드");
+    }
+}
+
+public class StaticMethod
+{
+    public static void main(String[] args)
+    {
+        A a1 = new A();
+        a1.abc();
+
+        A.bcd();
+        A a2 = new A();
+        a2.bcd();
+    }
+}
