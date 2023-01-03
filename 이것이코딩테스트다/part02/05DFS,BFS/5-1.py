e09cd9aafaf1766b11e0d31f4ec73868b7a6672e
n27 <arollingstone27@gmail.com>
2023-01-03 16:39:41 +0900

    05DFS,BFS

---
+++ 이것이코딩테스트다/part02/05DFS,BFS/5-1.py
@@ -0,0 +1,13 @@
+stack = []
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(1)
+stack.pop()
+
+print(stack)
+# 얘가 스택구조
+print(stack[::-1])
